a0b119816d631285cd5c4141d6e80bd64c7c31f6
Danil Tuzhik <danil-tuzhik@yandex.ru>
2020-11-18 23:58:50 +0700

    Modification

--- code.js/Normalize.js
+++ code.js/Normalize.js
@@ -1,10 +1,12 @@
 // BEGIN (write your solution here)
+
 const normalize = lesson => {
   lesson.name = _.capitalize(lesson.name);
   lesson.description = lesson.description.toLowerCase();
 }
 
 export default normalize;
+
 // END
 
 /*lesson.js
