49041894f2a77f4a844ed38713123329dcdfdbe6
Danil Tuzhik <danil-tuzhik@yandex.ru>
2020-11-18 23:48:17 +0700

    mv

---
+++ code.js/Objects.js
@@ -0,0 +1,12 @@
+// BEGIN (write your solution here)
+const fn = obj => {
+  return obj = {
+  "files": ["src/objects.js"],
+  "config": true
+  }
+}
+ export default fn;
+// END
+
+/*objects.js
+Реализуйте и экспортируйте по умолчанию функцию, которая возвращает ассоциативный массив, соответствующий JSON из файла example.json в этом упражнении. Всё, что вам нужно сделать — посмотреть на содержимое example.json и руками сформировать объект аналогичной структуры.*/
